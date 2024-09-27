228e4937654e21462775e27d9957755c2550dad7
Taranchuk <nfhfyxer@gmail.com>
2024-09-28 02:32:05 +0300

    add gizmo and inspect string

--- 1.5/Source/Hediff_FungalNexus.cs
+++ 1.5/Source/Hediff_FungalNexus.cs
@@ -8,16 +8,16 @@ namespace DanceOfEvolution
 	public class Hediff_FungalNexus : HediffWithComps
 	{
 		private int timer = 0;
-		private List<Pawn> burrowers = new();
-		private int MaxServants => 4;
+		public List<Pawn> servants = new();
+		public int MaxServants => 4;
 		public override void PostAdd(DamageInfo? dinfo)
 		{
-			base.PostAdd(dinfo); 
+			base.PostAdd(dinfo);
 			if (!pawn.Inhumanized())
 			{
 				pawn.health.AddHediff(HediffDefOf.Inhumanized);
 			}
-			burrowers = new();
+			servants = new();
 			timer = 0;
 		}
 
@@ -35,10 +35,36 @@ namespace DanceOfEvolution
 			}
 		}
 
+		public override IEnumerable<Gizmo> GetGizmos()
+		{
+			if (pawn.IsColonistPlayerControlled)
+			{
+				yield return new FungalNexusGizmo(this);
+			}
+			
+			if (DebugSettings.ShowDevGizmos)
+			{
+				yield return new Command_Action
+				{
+					defaultLabel = "DEBUG: Add Burrower",
+					action = () => SpawnBurrower(),
+				};
+			}
+		}
+
+		public override string GetInspectString()
+		{
+			if (servants.Count < MaxServants)
+			{
+				return "DE_NextBurrowerSpawnIn".Translate(((int)((GenDate.TicksPerDay * 2f) - timer)).ToStringTicksToPeriod());
+			}
+			return null;
+		}
+		
 		private void CheckAndUpdateBurrowers()
 		{
-			burrowers.RemoveAll(x => x is null || x.Destroyed || x.Dead);
-			if (burrowers.Count < MaxServants)
+			servants.RemoveAll(x => x is null || x.Destroyed || x.Dead);
+			if (servants.Count < MaxServants)
 			{
 				SpawnBurrower();
 			}
@@ -47,7 +73,7 @@ namespace DanceOfEvolution
 		private void SpawnBurrower()
 		{
 			var newBurrower = PawnGenerator.GeneratePawn(DefsOf.DE_Burrower, pawn.Faction);
-			burrowers.Add(newBurrower);
+			servants.Add(newBurrower);
 			GenSpawn.Spawn(newBurrower, pawn.Position, pawn.Map);
 		}
 
@@ -63,11 +89,11 @@ namespace DanceOfEvolution
 		public override void ExposeData()
 		{
 			base.ExposeData();
-			Scribe_Collections.Look(ref burrowers, "burrowers", LookMode.Reference);
+			Scribe_Collections.Look(ref servants, "servants", LookMode.Reference);
 			Scribe_Values.Look(ref timer, "timer");
 			if (Scribe.mode == LoadSaveMode.PostLoadInit)
 			{
-				burrowers ??= new();
+				servants ??= new();
 			}
 		}
 	}
