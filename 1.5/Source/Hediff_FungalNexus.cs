22f9abf51d301e2f05d707584b2ba6821be838ac
Taranchuk <nfhfyxer@gmail.com>
2024-09-27 14:17:27 +0300

    tweaks

--- 1.5/Source/Hediff_FungalNexus.cs
+++ 1.5/Source/Hediff_FungalNexus.cs
@@ -5,11 +5,18 @@ namespace DanceOfEvolution
 {
 	public class Hediff_FungalNexus : HediffWithComps
 	{
+		public override void PostAdd(DamageInfo? dinfo)
+		{
+			base.PostAdd(dinfo); 
+			if (!pawn.Inhumanized())
+			{
+				pawn.health.AddHediff(HediffDefOf.Inhumanized);
+			}
+		}
 		public bool IsImmuneTo(Hediff other)
 		{
 			if (HediffDefOf.LungRotExposure == other.def 
-				|| HediffDefOf.LungRot == other.def 
-				|| HediffDefOf.Inhumanized == other.def)
+				|| HediffDefOf.LungRot == other.def)
 			{
 				return true;
 			}
---
+++ 1.5/Source/ThoughtWorker_RotStink_CurrentStateInternal_Patch.cs
@@ -0,0 +1,18 @@
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace DanceOfEvolution
+{
+    [HarmonyPatch(typeof(ThoughtWorker_RotStink), nameof(ThoughtWorker_RotStink.CurrentStateInternal))]
+	public static class ThoughtWorker_RotStink_CurrentStateInternal_Patch
+	{
+		public static void Postfix(ref ThoughtState __result, Pawn pawn)
+		{
+			if (pawn.HasFungalNexus())
+			{
+				__result = ThoughtState.Inactive;
+			}
+		}
+	}
+}
