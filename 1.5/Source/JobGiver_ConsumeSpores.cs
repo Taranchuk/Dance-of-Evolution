d3336c0c1093ecfff69dbe590db0eab5b0747569
Taranchuk <nfhfyxer@gmail.com>
2024-11-05 20:30:35 +0200

    fixes

--- 1.5/Source/JobGiver_ConsumeSpores.cs
+++ 1.5/Source/JobGiver_ConsumeSpores.cs
@@ -8,7 +8,7 @@ namespace DanceOfEvolution
 	{
 		public override Job TryGiveJob(Pawn pawn)
 		{
-			if (pawn.IsServant() || pawn.HasFungalNexus())
+			if (pawn.IsControllableServant() || pawn.HasFungalNexus())
 			{
 				var sporeMakers = pawn.Map.listerThings.ThingsOfDef(DefsOf.DE_Sporemaker)
 					.Concat(pawn.Map.listerThings.ThingsOfDef(DefsOf.DE_HardenedSporemaker)).Cast<Building_Sporemaker>();
